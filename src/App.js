02b791088642ca739fc84ceb62c3afe8e659a5e4
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-05-22 17:14:38 -0300

    alfala

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import Logo from './components/Logo';
 import './App.css';
+import OptionsHeader from './components/OptionsHeader';
+import IconeHeader from './components/IconesHeader';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">    
-        <Logo /> 
-        <ul className='App-header'>
-        <li className='opcao'><p>CATEGORIAS</p></li>
-          <li className='opcao'><p>MINHA ESTANTE</p></li>
-          <li className='opcao'><p>FAVORITOS</p></li>
-        </ul>
+      <header className='App-header'>
+        <Logo />
+        <OptionsHeader />
+        <IconeHeader />
 
       </header>
     </div>
   );
 }
+
 
 export default App;
